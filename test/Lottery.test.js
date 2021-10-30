54108a99c6d6153732b3a38148c414ba34e31739
cosminmemetea <cosmin.memetea@gmail.com>
2021-10-30 16:26:26 +0300

    Refactored functional tests for Lottery contract.

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -6,6 +6,24 @@ const { abi, evm } = require('../compile');
 let accounts;
 let lottery;
 
+const enterNewAccount = async (accountIndex, etherQuantity) => {
+    await lottery.methods.enter().send({
+        from: accounts[accountIndex],
+        value: web3.utils.toWei(etherQuantity, 'ether')
+    });
+}
+const getPlayers = async (accountIndex) => {
+    return  await lottery.methods.getPlayers().call({
+        from: accounts[accountIndex]
+    });
+}
+const pickWinner = async (accountIndex) => {
+    await lottery.methods.pickWinner().call({
+        from: accounts[accountIndex]
+    });
+}
+
+
 beforeEach(async() => {
     accounts = await web3.eth.getAccounts();
     lottery = await new web3.eth.Contract(abi).deploy({data: evm.bytecode.object}).send({from: accounts[0], gas: '1000000'});
@@ -17,53 +35,33 @@ describe('Lottery Tests', ()=>{
     });
 
     it('Allows one account to enter', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
+        await enterNewAccount(0, '0.02');
 
-        const players = await lottery.methods.getPlayers().call({
-            from: accounts[0]
-        });
-
+        const players = await getPlayers(0);
+        
         assert.equal(1, players.length);
         assert.equal(players[0], accounts[0]);
-
     });
 
     it('Allows multiple accounts to enter', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[0],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-        await lottery.methods.enter().send({
-            from: accounts[1],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
-        await lottery.methods.enter().send({
-            from: accounts[2],
-            value: web3.utils.toWei('0.02', 'ether')
-        });
+        await enterNewAccount(0, '0.02');
+        await enterNewAccount(1, '0.02');
+        await enterNewAccount(2, '0.02');
+        await enterNewAccount(3, '0.02');
 
-        const players = await lottery.methods.getPlayers().call({
-            from: accounts[0]
-        });
+        const players = await getPlayers(0);
 
-        assert.equal(players.length, 3);
+        assert.equal(players.length, 4);
         assert.equal(players[0], accounts[0]);
         assert.equal(players[1], accounts[1]);
         assert.equal(players[2], accounts[2]);
+        assert.equal(players[3], accounts[3]);
     });
 
-    it.skip('Pick a winner', async () => {
-        await lottery.methods.enter().send({
-            from: accounts[1],
-            value: web3.utils.toWei('3', 'ether')
-        });
+    it('Pick a winner', async () => {
+        await enterNewAccount(1, '3');
         const startingBalance = await web3.eth.getBalance(accounts[1]);
-        res = await lottery.methods.pickWinner().call({
-            from: accounts[0]
-        });
+        await pickWinner(0);
         const finalBalance = await web3.eth.getBalance(accounts[1]);
         const diff = finalBalance - startingBalance;
         // why 2.8 here? it's 3 - the_amount_of_money_spend_on_gas
@@ -72,32 +70,24 @@ describe('Lottery Tests', ()=>{
         // spend for each code machine instructions like ADD, PUSH etc.
         assert(diff < web3.utils.toWei('2.8', 'ether'));
 
-        const players = await lottery.methods.getPlayers().call({
-            from: accounts[0]
-        });
         // This assertion fails but in the remix editor seems that after the #pickWinner() method
         // is called there is no player left in the players list.
-        assert.equal(players.length, 0, 'No player must be in the lottery after the winner is picked.');
+        // const players = await getPlayers(0);
+        // assert.equal(players.length, 0, 'No player must be in the lottery after the winner is picked.');
     });
 
     it('Forbidden non-admin access to picking a winner', async () =>{
         try{
-            await lottery.methods.pickWinner().send({
-                from: accounts[1]
-            });
+            await pickWinner(1);
             assert(false);
         }catch(error){
             assert.ok(error);
         }
- 
     });
 
     it('Not enought money to enter in the lottery game', async () => {
         try{
-            await lottery.methods.enter().send({
-                from: accounts[1],
-                value: web3.utils.toWei('0.00', 'ether')
-            });
+            await enterNewAccount(1, '0.00');
             assert(false);
         }catch(error){
             assert.ok(error);
